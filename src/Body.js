382577f4452a874c171557886717f032d9e9b57f
merewelch214 <merewelch@gmail.com>
2021-01-12 17:30:45 -0500

    inital commit

---
+++ src/Body.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Body = () => {
+    return (
+        <div>This is where the body will go</div>
+    )
+}
+
+export default Body;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>This is where the header will go</div>
+    )
+}
+
+export default Header;
