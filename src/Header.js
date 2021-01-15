73a9de8f83606bddb028c1c153324b8170cc0934
merewelch214 <merewelch@gmail.com>
2021-01-14 20:29:19 -0500

    add header styling

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import moment from 'moment';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Title = styled.div`
+    font-family: Merriweather;
+    font-size: 2rem;
+    text-align: center;
+    padding-top: 2rem;
+    padding-bottom: 0;
+    margin: 0;
+`;
+
+const Subtitle = styled.div`
+    font-family: Open Sans; 
+    font-size: 1rem;
+    text-align: center;
+    padding: .3rem;
+    margin: 0;
+`
 
 function Header() {
     return (
-        <div>
-            <h1>News Sentiment Analyzer</h1>
-            <h2>Today's date is {moment().format('MMMM Do YYYY')}</h2>
-        </div>
+        <HeaderContainer>
+            <Title>News Sentiment Analyzer</Title>
+            <Subtitle>Today's date is {moment().format('MMMM Do YYYY')}</Subtitle>
+        </HeaderContainer>
     )
 }
 
